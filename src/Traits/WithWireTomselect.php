512435a6a3cc3f0dc8ba8eae294fde5c279e3495
S M Rishad <rishad.black@gmail.com>
2025-01-04 03:47:41 +0600

    Fix Bugs

---
+++ src/Traits/WithWireTomselect.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Rishadblack\WireTomselect\Traits;
+
+trait WithWireTomselect
+{
+    public function mountWithWireTomselect($data = [])
+    {
+        if (isset($data['data']) && isset($data['data']['text']) && !empty($data['data']['text'])) {
+            session()->put('tom_select_remote', $data['data']);
+
+            if (property_exists($this, 'name')) {
+                $this->name = $data['data']['text']; // Assign the value of $name to $tom_select_remote_text
+            }
+
+            if (method_exists($this, 'tomSelectText')) {
+                // Dynamically call the method and pass the data
+                $this->tomSelectText($data['data']['text']);
+            }
+        }
+
+    }
+
+    public function tomSelectRemoteUpdate(string | int $id, string $name)
+    {
+        $remoteData = session()->get('tom_select_remote');
+        $fieldName = $remoteData['field_name'] ?? null;
+
+        if (!$fieldName) {
+            return true;
+        }
+
+        session()->forget(['tom_select_remote']);
+
+        $this->tomSelectUpdate([
+            $fieldName => [
+                'value' => $id,
+                'options' => [
+                    'id' => $id,
+                    'name' => $name,
+                ],
+            ],
+        ]);
+
+        return false;
+    }
+
+    public function tomSelectUpdate(array $options)
+    {
+        $this->dispatch('tom_select_set_value', $options);
+    }
+}
